a78f67d2bc6930c772f7a1d0b96d5b7a656741ba
SRYVarder <smallreezy3@gmail.com>
2025-09-28 10:45:35 +0300

    Update pagination.py

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,5 +1,6 @@
-
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
 
 class MessagePagination(PageNumberPagination):
     # The API will fetch 20 messages per page by default
@@ -9,4 +10,12 @@ class MessagePagination(PageNumberPagination):
     # Allows client to override page size: ?page_size=X, with a max of 100
     page_size_query_param = 'page_size'
     max_page_size = 100
-    # You can also customize the response format if needed
+
+    def get_paginated_response(self, data):
+        # Customize the response to include page.paginator.count
+        return Response({
+            'count': self.page.paginator.count,  # Total number of items
+            'next': self.get_next_link(),        # URL to next page
+            'previous': self.get_previous_link(), # URL to previous page
+            'results': data                      # Paginated data
+        })
