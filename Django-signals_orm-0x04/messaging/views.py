21800fcd005e36f907efabe2240d655fa00548ad
SRYVarder <smallreezy3@gmail.com>
2025-10-05 23:48:05 +0300

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -39,7 +39,8 @@ def delete_user(request):
 
 @login_required
 def unread_messages(request):
-    messages = Message.objects.filter(receiver=request.user).select_related('sender').only('id', 'sender', 'content', 'created_at')
+    # ✅ This line is required and now valid
+    messages = Message.unread_objects.unread_for_user(request.user)
     return render(request, 'messaging/unread_messages.html', {'messages': messages})
 
 
