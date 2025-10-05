0a94b2046bb94bfe07464b56a1ec551753d6c827
SRYVarder <smallreezy3@gmail.com>
2025-10-05 19:26:31 +0300

    Add files via upload

---
+++ Django-signals_orm-0x04/messaging/messaging/messaging/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+
+from .models import Message
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_page
+
+
+def message_history(request, message_id):
+    message = Message.objects.get(id=message_id)
+    history = message.history.all()
+    return render(request, 'messaging/message_history.html', {
+        'message': message,
+        'history': history
+    })
+
+def conversation_thread(request, message_id):
+    message = Message.objects.select_related('sender', 'receiver').prefetch_related('replies').get(id=message_id)
+    # Recursive function to collect all replies
+    def get_replies(message, replies_list):
+        replies = message.replies.select_related('sender', 'receiver').prefetch_related('replies')
+        for reply in replies:
+            replies_list.append(reply)
+            get_replies(reply, replies_list)
+        return replies_list
+
+    all_replies = get_replies(message, [])
+    return render(request, 'messaging/conversation_thread.html', {
+        'message': message,
+        'replies': all_replies
+    })
+
+@login_required
+def delete_user(request):
+    user = request.user
+    user.delete()
+    return HttpResponse("User account deleted")
+
+@login_required
+def unread_messages(request):
+    messages = Message.unread_objects.unread_for_user(request.user)
+    return render(request, 'messaging/unread_messages.html', {'messages': messages})
+
+@cache_page(60)  # Cache for 60 seconds
+@login_required
+def message_list(request):
+    messages = Message.objects.filter(receiver=request.user).select_related('sender')
+    return render(request, 'messaging/message_list.html', {'messages': messages})
