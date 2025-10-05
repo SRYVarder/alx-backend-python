08767c8a36559a56ad9991519dc0781c6f579aab
SRYVarder <smallreezy3@gmail.com>
2025-10-05 22:21:39 +0300

    Update models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -50,4 +50,4 @@ class Notification(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Notification for {self.user} - Message ID {self.message.id}"
+         return f"Notification for {self.user} about message {self.message.id}"
